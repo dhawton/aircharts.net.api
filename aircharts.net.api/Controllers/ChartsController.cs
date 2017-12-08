bcc9b5ef0cd9fe61561301c060e17c7ec6032776
Daniel Hawton <daniel@hawton.com>
2017-12-08 00:09:06 -0900

    Remove global chart list, add todo

--- aircharts.net.api/Controllers/ChartsController.cs
+++ aircharts.net.api/Controllers/ChartsController.cs
@@ -10,7 +10,7 @@ using aircharts.net.api.Models;
 namespace aircharts.net.api.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Charts")]
+    [Route("v2/")]
     public class ChartsController : Controller
     {
         private readonly AirchartContext _context;
@@ -20,15 +20,9 @@ namespace aircharts.net.api.Controllers
             _context = context;
         }
 
-        // GET: api/Charts
-        [HttpGet]
-        public IEnumerable<Charts> GetCharts()
-        {
-            return _context.Charts;
-        }
-
-        // GET: api/Charts/airport
-        [HttpGet("{id}")]
+        // GET: /v2/Airport/{id}
+        // @TODO : Add support for multiple airports in list, IE: {id},{id},{id}
+        [HttpGet("Airport/{id}")]
         public IActionResult GetCharts([FromRoute] string id)
         {
             if (!ModelState.IsValid)
@@ -42,77 +36,6 @@ namespace aircharts.net.api.Controllers
             {
                 return NotFound();
             }
-
-            return Ok(charts);
-        }
-
-        // PUT: api/Charts/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCharts([FromRoute] string id, [FromBody] Charts charts)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != charts.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(charts).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChartsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Charts
-        [HttpPost]
-        public async Task<IActionResult> PostCharts([FromBody] Charts charts)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Charts.Add(charts);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetCharts", new { id = charts.Id }, charts);
-        }
-
-        // DELETE: api/Charts/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCharts([FromRoute] string id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var charts = await _context.Charts.SingleOrDefaultAsync(m => m.Id == id);
-            if (charts == null)
-            {
-                return NotFound();
-            }
-
-            _context.Charts.Remove(charts);
-            await _context.SaveChangesAsync();
 
             return Ok(charts);
         }
